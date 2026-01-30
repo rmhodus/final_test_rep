f2b5e59b59a0a764fd77c540982681a7e8e741d8
Roman <romankhodusmail@gmail.com>
2026-01-30 09:42:15 +0100

    Update greeting message in index.js

--- app/index.js
+++ app/index.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Node.js app deployed via AWS CI/CD pipelin!');
+  res.send('Hello from the Node.js app deployed via AWS CI/CD pipelin! HELLO FROM GERMANY');
 });
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
+
 
