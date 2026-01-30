0e4110e59f5904126488ab3e9a7bc63d368ec4a1
Roman <romankhodusmail@gmail.com>
2026-01-30 08:52:18 +0100

    Update after_install.sh to include ECR login and cleanup
    Added ECR login and cleanup steps before starting the app.

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# 1. Авторизация в ECR
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 004226106633.dkr.ecr.us-east-1.amazonaws.com
 
-# Зупиняємо старий застосунок
+# 2. Остановка и удаление старого контейнера (если он есть)
 docker stop my-nodejs-app || true
 docker rm my-nodejs-app || true
 
-# Запускаємо новий застосунок
-docker run -d --name my-nodejs-app -p 80:8080 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-nodejs-app:latest
+# 3. Удаление старых образов для экономии места
+docker system prune -af
+
+# 4. Запуск нового контейнера
+docker run -d --name my-nodejs-app -p 80:8080 004226106633.dkr.ecr.us-east-1.amazonaws.com/my-nodejs-app:latest
